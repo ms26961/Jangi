34f3a64a4241f570a7abbae816e666e1316f3b9d
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-02-26 19:18:47 -0500

    Update LAB5ex5.py

--- ECET411/LAB5ex5.py
+++ ECET411/LAB5ex5.py
@@ -1,16 +1,23 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
+# Set up GPIO using BCM numbering
 GPIO.setmode(GPIO.BCM)
-LED = 21
-GPIO.setup(LED, GPIO.OUT)
+
+# Define the GPIO pin for the LED
+LED_PIN = 21
+GPIO.setup(LED_PIN, GPIO.OUT)
+
 while True:
-    numBlinks = int(input('Enter Number of Blinks: '))
-    delayOn = float(input('Enter DelayOn: '))
-    delayOff = float(input('Enter DelayOff: '))
-    for i in range(0, numBlinks, 1):
-        GPIO.output(LED, GPIO.HIGH)
+    num_blinks = int(input('Enter number of blinks: '))
+    on_duration = float(input('Enter ON duration (seconds): '))
+    off_duration = float(input('Enter OFF duration (seconds): '))
+    
+    for _ in range(num_blinks):
+        GPIO.output(LED_PIN, GPIO.HIGH)
         print('LED ON')
-        sleep(delayOn)
-        GPIO.output(LED, GPIO.LOW)
+        sleep(on_duration)
+        
+        GPIO.output(LED_PIN, GPIO.LOW)
         print('LED OFF')
-        sleep(delayOff)
+        sleep(off_duration)
