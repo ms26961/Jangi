1584aa7d7cf71c993cec08b8f4ca5f91422b2e1f
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-03-04 20:00:55 -0500

    Create Lab6Encoder.py

---
+++ ECET411/Lab6Encoder.py
@@ -0,0 +1,31 @@
+#encoder
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+CLK = 17
+DT = 27
+SW = 22
+GPIO.setup(CLK, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DT, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+clk_prev = 0
+encode_count = 0
+while True:
+        clk_status = GPIO.input(CLK)
+        dt_status = GPIO.input(DT)
+        sw_status = GPIO.input(SW)
+        if sw_status == 0:
+                encode_count = 0
+                print('encode_count = ', encode_count)
+
+        if clk_status !=clk_prev:
+                if clk_status == dt_status:
+                        encode_count  = encode_count  + 1
+                else:
+                         encode_count  = encode_count - 1
+                if clk_prev == 1:
+                        clk_prev = 0
+                else:
+                        clk_prev  = 1
+        print('Encoder pulses = ', encode_count)
