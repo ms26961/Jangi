ee089320cef3e3b112220f2ed0533b7922a60bea
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-03-18 19:07:54 -0400

    Update Lab7StepperMotor.py

--- ECET411/Lab7StepperMotor.py
+++ ECET411/Lab7StepperMotor.py
@@ -2,44 +2,41 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)  # Ignore warning for now
-step_pads = [19, 13, 6, 5]
+stepPads = [19, 13, 6, 5]
 GPIO.setmode(GPIO.BCM)
 
-for i in range(4):
-    GPIO.setup(step_pads[i], GPIO.OUT)
+for i in range(0, 4):
+    GPIO.setup(stepPads[i], GPIO.OUT)
 
-def motor_drive(number_steps, frequency, direction):
-    speed = 1 / frequency
-
-    if direction == 'R':
-        for k in range(number_steps):
+def motorDrive(numberSteps, Frequency, Direction):
+    speed = float(1 / Frequency)
+    if Direction == 'R':
+        for k in range(0, numberSteps):
             for x in range(3, -1, -1):
                 for j in range(3, -1, -1):
                     if x == j:
-                        GPIO.output(step_pads[j], True)
+                        GPIO.output(stepPads[j], True)
                         print("Step Number = ", k)
                     else:
-                        GPIO.output(step_pads[j], False)
-                print("Reverse")
+                        GPIO.output(stepPads[j], False)
+                        print("Reverse")
                 time.sleep(speed)
-
-    elif direction == 'F':
-        for k in range(number_steps):
-            for x in range(4):
-                for j in range(4):
+    elif Direction == 'F':
+        for k in range(0, numberSteps):
+            for x in range(0, 4, 1):
+                for j in range(0, 4, 1):
                     if x == j:
-                        GPIO.output(step_pads[j], True)
+                        GPIO.output(stepPads[j], True)
                         print("Step Number = ", k)
                     else:
-                        GPIO.output(step_pads[j], False)
-                print("Forward")
+                        GPIO.output(stepPads[j], False)
+                        print("Forward")
                 time.sleep(speed)
-
     else:
         print("WRONG DIRECTION ENTERED!")
 
 while True:
-    num_steps = int(input("Enter Number of Steps: "))
-    freq = float(input("Enter Frequency: "))
-    direction = input("Enter Direction: ")
-    motor_drive(num_steps, freq, direction)
+    numSteps = int(input("Enter Number of Steps: "))
+    Freq = float(input("Enter Frequency: "))
+    Dir = input("Enter Direction: ")
+    motorDrive(numSteps, Freq, Dir)
