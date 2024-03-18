d0a7482a23b6365be11bc1b621d3f90c65907e29
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-03-18 13:35:52 -0400

    Update Lab6Encoder.py

--- ECET411/Lab6Encoder.py
+++ ECET411/Lab6Encoder.py
@@ -1,33 +1,36 @@
-#encoder
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-CLK = 18
-DT = 24
-SW = 23
-GPIO.setup(CLK, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(DT, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(SW, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-clk_prev = 0
-encode_count = 0
-while True:
-        clk_status = GPIO.input(CLK)
-        dt_status = GPIO.input(DT)
-        sw_status = GPIO.input(SW)
-        if sw_status == 0:
-                encode_count = 0
-                print('encode_count = ', encode_count)
 
-        if clk_status !=clk_prev:
-                if clk_status == dt_status:
-                        encode_count  = encode_count  + 1
-                else:
-                         encode_count  = encode_count - 1
-                if clk_prev == 1:
-                        clk_prev = 0
-                else:
-                        clk_prev  = 1
+CLK_PIN = 18
+DT_PIN = 24
+SW_PIN = 23
+
+GPIO.setup(CLK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(SW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+clk_prev_state = 0
+encoder_count = 0
+
+while True:
+    clk_state = GPIO.input(CLK_PIN)
+    dt_state = GPIO.input(DT_PIN)
+    sw_state = GPIO.input(SW_PIN)
+    
+    if sw_state == 0:
+        encoder_count = 0
+        print('Resetting encoder count to 0')
+        
+    if clk_state != clk_prev_state:
+        if clk_state == dt_state:
+            encoder_count += 1
+        else:
+            encoder_count -= 1
+        
+        clk_prev_state = 1 if clk_prev_state == 0 else 0
                         
-        print('Encoder pulses = ', encode_count)
-        sleep(.005)
+    print('Encoder pulses:', encoder_count)
+    sleep(0.005)
