ac776da6c9e96ee6eb3e46a39252f8f8ef34857e
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-03-18 13:37:03 -0400

    Update Lab6WaterSensor.py

--- ECET411/Lab6WaterSensor.py
+++ ECET411/Lab6WaterSensor.py
@@ -1,15 +1,16 @@
- import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 23
-GPIO.setup(PIR_PIN, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+pir_sensor_pin = 23
+GPIO.setup(pir_sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-
-        PIR_status = GPIO.input(PIR_PIN)
-        if PIR_status == 1:
-                print('/////WET')
-        else:
-                print('//////////DRY')
-        sleep(.1)
+    pir_status = GPIO.input(pir_sensor_pin)
+    if pir_status:
+        print('-> Moist')
+    else:
+        print('-> Dry')
+    sleep(0.1)
