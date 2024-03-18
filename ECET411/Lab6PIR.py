b4e5181cdbc31a9b550d9c4101617646187b4a7d
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-03-18 13:36:24 -0400

    Update Lab6PIR.py

--- ECET411/Lab6PIR.py
+++ ECET411/Lab6PIR.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 4
-GPIO.setup(PIR_PIN, GPIO.IN)
+
+SENSOR_PIN = 4
+GPIO.setup(SENSOR_PIN, GPIO.IN)
 
 while True:
-
-        PIR_status = GPIO.input(PIR_PIN)
-        if PIR_status == 1:
-                print('Moved')
-        sleep(.1)
+    sensor_status = GPIO.input(SENSOR_PIN)
+    
+    if sensor_status == 1:
+        print('Motion detected')
+    
+    sleep(0.1)
