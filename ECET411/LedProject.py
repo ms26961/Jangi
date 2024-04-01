f041bd0cd3002c960b498a72e1447d690cb04980
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-04-01 19:35:59 -0400

    Update LedProject.py

--- ECET411/LedProject.py
+++ ECET411/LedProject.py
@@ -24,7 +24,7 @@ def greenClick():
         greenLabel.config(text="Green LED OFF", bg="green")
         GPIO.output(LED, GPIO.LOW)  # Turn Off
 
-greenLedButton = Button(root, text="GREEN", command=greenClick, padx=20, pady=10, fg="white", bg="green")
+greenLedButton = Button(root, text="GREEN", command=greenClick, padx=50, pady=25, fg="white", bg="green")
 greenLedButton.grid(row=3, column=5)
 
 greenLabel = Label(root, text="Green LED OFF", fg="white", bg="green")
