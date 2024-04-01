9e18c06ef6dfc1d9c44b7c5ccbd7571a64e467fa
ms26961 <159856068+ms26961@users.noreply.github.com>
2024-04-01 19:26:21 -0400

    Update LedProject.py

--- ECET411/LedProject.py
+++ ECET411/LedProject.py
@@ -1,24 +1,38 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep func�on from the �me module
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+import RPi.GPIO as GPIO
+from time import sleep
+from tkinter import *
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
 LED = 21
-GPIO.setup(LED, GPIO.OUT, inial=GPIO.LOW) # Set pin 21 to be an output pin
-from tkinter import * 
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
+
 root = Tk()
 root.title("LED Project")
 root.geometry('600x400')
+
 Count = 0
-def greenClick(): global Count Count = Count + 1 counterReminder = Count % 2 if counterReminder == 1: greenLabel = Label(root, text = "Green LED ON", fg = "white", bg ="green") greenLabel.grid(row=7, column=5)
- GPIO.output(LED, GPIO.HIGH) # Turn on
- else: greenLabel = Label(root, text = "Green LED OFF", fg = "white", bg ="green") greenLabel.grid(row=7, column=5)
- GPIO.output(LED, GPIO.LOW) # Turn Off
-greenLedButton = Button(root, text="GREEN",command=lambda:greenClick(), padx = 10,
-pady = 0, fg = "white", bg = "green")
-greenLabel = Label(root, text = "Green LED OFF", fg = "white", bg ="green")
-root.columnconfigure(5, weight=1)
-root.rowconfigure(7, weight = 1)
-root.rowconfigure(3, weight = 1)
+
+def greenClick():
+    global Count
+    Count += 1
+    counterReminder = Count % 2
+    if counterReminder == 1:
+        greenLabel.config(text="Green LED ON", bg="green")
+        GPIO.output(LED, GPIO.HIGH)  # Turn on
+    else:
+        greenLabel.config(text="Green LED OFF", bg="green")
+        GPIO.output(LED, GPIO.LOW)  # Turn Off
+
+greenLedButton = Button(root, text="GREEN", command=greenClick, padx=10, pady=0, fg="white", bg="green")
 greenLedButton.grid(row=3, column=5)
+
+greenLabel = Label(root, text="Green LED OFF", fg="white", bg="green")
 greenLabel.grid(row=7, column=5)
+
+root.columnconfigure(5, weight=1)
+root.rowconfigure(7, weight=1)
+root.rowconfigure(3, weight=1)
+
 root.mainloop()
